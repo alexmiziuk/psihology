570a037ddbfdde71817c7b58abf5bc4fda5c3539
Alex <muzikbasss@gmail.com>
2024-09-09 08:21:33 +0200

    feat: add favicon and Meta Tags

--- src/modules/navMenu.js
+++ src/modules/navMenu.js
@@ -1,52 +1,4 @@
-
-/* document.addEventListener('DOMContentLoaded', function () { */
-
 // Dropdown menu
-
-/* 	const menuItems = ['my-expertise', 'my-works', 'contacts'];
-	// Находим все элементы с классом .nav__menu-item--dropdown
-	menuItems.forEach(itemId => {
-		const menuItem = document.getElementById(itemId);
-
-		 menuItem.addEventListener('mouseenter', function () {
-			const submenu = this.querySelector(`.submenu-${itemId}`);
-			submenu.style.display = 'block';
-			submenu.style.transition = 'all 1s ease';
-			setTimeout(() => {
-				submenu.style.opacity = '1';
-				submenu.style.visibility = 'visible';
-				submenu.style.zIndex = '1';
-			}, 100);
-		});
-
-		menuItem.addEventListener('mouseleave', function () {
-			const submenu = this.querySelector(`.submenu-${itemId}`);
-			submenu.style.opacity = '0';
-			submenu.style.visibility = 'hidden';
-			setTimeout(() => {
-				submenu.style.display = 'none';
-			}, 600);
-		});
-
-		menuItem.addEventListener('click', function (event) {
-			event.stopPropagation();
-			const submenu = this.querySelector(`.submenu-${itemId}`);
-			submenu.style.display = 'block';
-			submenu.style.transition = 'all 0.6s ease';
-			setTimeout(() => {
-				submenu.style.opacity = '1';
-				submenu.style.visibility = 'visible';
-				submenu.style.zIndex = '1';
-			}, 100);
-		});
-	}); */
-
-
-
-
-
-/* }); */
-
 
 document.addEventListener('DOMContentLoaded', function () {
 	const menuItems = ['my-expertise', 'my-works', 'contacts'];
@@ -70,8 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		menuItem.addEventListener('mouseleave', function () {
 			clearTimeout(enterTimeout); // Очищаем таймер входа
 			const submenu = this.querySelector(`.submenu__${itemId}`);
-
-			leaveTimeout = setTimeout(() => {
+			enterTimeout = setTimeout(() => {
 				submenu.style.opacity = '0';
 				submenu.style.visibility = 'hidden';
 				submenu.style.zIndex = '-1';
